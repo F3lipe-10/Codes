680526da593eff96ec8141b0db0df2f327d1559e
F3lipe-10 <andres.benavides.r08@gmail.com>
2024-10-22 10:29:34 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,26 +1,20 @@
 #Hola
 import streamlit as st
-import math
+from scipy.optimize import fsolve
+import numpy as np
 
 st.set_page_config(page_title="Calculadora reposicionamiento", layout="wide")
 
 with st.container():
     st.title("Calculadora de Reposicionamientos")
-    st.write("Bienvenido(a)! Con esta calculadora podrá saber cada cuanto tiempo se debe reposicionar un paciente en una unidad de cuidados intensivos, a continuación se solicitarán datos del paciente, con los cuales se calculará el tiempo. Además se le solicitará que haga la valoración de la escala de Braden para el calculo completo. ¡Adelante!")
+    st.write("Bienvenido(a)! Con esta calculadora podrá saber la frecuencia de reposicionamientos de paciente en una unidad de cuidados intensivos, a continuación se solicitarán datos del paciente, con los cuales se calculará la frecuencia. Además se le solicitará que haga la valoración de la escala de Braden para el calculo completo. ¡Adelante!")
 st.markdown("---")
-
 
 edad = st.number_input("Escriba la edad del paciente")
 IMC = st.number_input("Escriba el IMC del paciente")
 braden_valores = []
-Patologias = {"Diabetes":'Diabetes Melitus',
-              "Endocrino":'Enfermedad endocrina',
-              "Cardiovascular":'Enfermedad cardiaca',
-              "Hepatitis":'Enfermedad hepatica',
-              "Respiratorio":'Enfermedad respiratoria',
-              "Renal":'Enfermedad renal', 
-              "Neurologico":'Problema neurologico'
-              }
+ECV1 = st.selectbox("Si el paciente presenta ECV, escogja 1, de lo contrario, escoja 0", options=(0.0,1.0))
+btn2 = st.button("Guardar datos")
 
 def clicked():
     braden_valores.append(braden)
@@ -29,37 +23,12 @@ def clicked():
     braden_valores.append(braden4)
     braden_valores.append(braden5)
     braden_valores.append(braden6)
-    riesgos(edad, IMC, braden_valores,pato2)
-    he, me, s, r = riesgos(edad, IMC, braden_valores,pato2)
-    st.header("El paciente presenta un riesgo de aparición de Úlceras por presión de:")
-    st.subheader(round(r,3))
-    if r > 0.9:
-        st.header("El paciente debe ser movilizado cada hora, su estado de salud es demasiado delicado")
-    else:
-        st.header("A este paciente se le debe hacer un reposicionamiento cada:")
-        with st.container():
-            a, b, c, d, e, f = st.columns(6)
-            with a:
-                st.write(he)
-            with b:
-                st.text("Hora(s)")
-            with c:
-                st.write(me)    
-            with d:
-                st.text("Minuto(s)")
-            with e:
-                st.write(s)
-            with f:
-                st.text("Segundo(s)")
+    riesgos(edad, IMC, braden_valores,ECV1)
+    promedionoches_solucion = riesgos(edad, IMC, braden_valores,ECV1)
+    st.subheader(f"Por noche, el paciente debe ser reposicionado: {promedionoches_solucion} veces")
     st.markdown("---")            
 
-pato1 = st.multiselect("Escoja la categoría de complicación que presenta el paciente", options=("Diabetes", "Cardiovascular", "Renal", "Respiratorio", "Neurologico", "Hepatitis", "Endocrino"))
-pato2 = []
-for patologia in pato1:
-    if patologia in Patologias:        
-        pato2.append(Patologias[patologia])
 
-st.multiselect("Escoja la complicación asociada a la categoría que escogió",options = (pato2))
 st.markdown("---")
 
 
@@ -75,37 +44,7 @@ with st.container():
     with datos2: 
         st.image("Escala-de-Braden.jpg", caption=("Imagen tomada de https://enfermeriacreativa.com/2019/01/28/escala-de-braden/"))
 
-def riesgos(edad, IMC, braden_valores,pato2):
-    horas_enteras = 0
-    minutos_enteros = 0
-    minutos_float = 0
-    segundos = 0
-    DM1,Ene,Enc,Enfhep,Enren, Pn , Enresp = [0]*7
-    patologia_dict = {
-        "Diabetes Melitus 1": "DM1",
-        "Enfermedad endocrina": "Ene",
-        "Enfermedad cardiaca": "Enc",
-        "Enfermedad hepática": "Enfhep",
-        "Enfermedad renal": "Enren",
-        "Enfermedad respiratoria": "Enresp",
-        "Problema neurológico": "Pn"
-    }
-    variables = {
-        "DM1": DM1,  "Ene": Ene, "Enc": Enc,
-        "Enfhep": Enfhep, "Enren": Enren, "Pn": Pn,
-        "Enresp": Enresp
-    }
-    for patologia in pato2:
-        if patologia in patologia_dict:
-            variables[patologia_dict[patologia]] = 1
-
-    DM1, Ene, Enc, Enfhep, Enren, Pn, Enresp = \
-        variables["DM1"], variables["Ene"], variables["Enc"], \
-        variables["Enfhep"], variables["Enren"], variables["Pn"], variables["Enresp"]
-    
- 
-    u = -7.366 - 0.01*edad + 0.179*IMC + 0.326*Enresp - 19.194*DM1 + 0.215*Pn - 18.574*Enc + 0.125*Enren -18.586*Enfhep + 0.713*Ene
-    Riesgo_total = ((math.exp(u))/(1+math.exp(u)))
+def riesgos(edad, IMC, braden_valores,ECV1):
 
     suma1 = sum(braden_valores)
     if suma1 < 12:
@@ -114,20 +53,36 @@ def riesgos(edad, IMC, braden_valores,pato2):
         riesgo_braden = "Medio"
     elif suma1 >= 14 and suma1 <= 23:
         riesgo_braden = "Bajo"
-    if riesgo_braden == "Alto":
-        Riesgo_total = Riesgo_total + 0.1
-    elif riesgo_braden == "Bajo":
-        Riesgo_total = Riesgo_total - 0.01
-    elif riesgo_braden == "Medio":
-        Riesgo_total = Riesgo_total - 0.025
+    #calculo
+    intercept = -1.802
+    coef_promedionoches = -0.479
+    coef_edad = 0.04
+    coef_ecv = 1.401
+    coef_braden_malta = 0.525
+    coef_obesidad = 3.751
 
-    Riesgo_total1 = 3- (3*Riesgo_total)
-    horas_enteras = int(Riesgo_total1)
-    minutos_float = (Riesgo_total1 - horas_enteras) * 60
-    minutos_enteros = int(minutos_float)
-    segundos = int((minutos_float - minutos_enteros) * 60)
-    DM1,Ene,Enc,Enfhep,Enren,Pn,Enresp = [0]*7
-    return horas_enteras, minutos_enteros, segundos, Riesgo_total
+    if IMC > 35 and IMC< 39.999:
+        obesidad = 1
+    else:
+        obesidad = 0    
+
+    if riesgo_braden == "Alto":
+        braden_malta = 1
+    else:
+        braden_malta = 0
+
+    logit_base = (intercept +
+                coef_edad * edad +
+                coef_ecv * ECV1 +
+                coef_braden_malta * braden_malta +
+                coef_obesidad * obesidad)
+    riesgo_base = 1 / (1 + np.exp(-logit_base))
+    riesgo_80 = 0.80 * riesgo_base
+
+        
+    promedionoches_solucion = -(np.log((1 / riesgo_80) - 1) + intercept + coef_edad * edad + coef_ecv * ECV1 + coef_braden_malta * braden_malta + coef_obesidad * obesidad) / coef_promedionoches
+    promedionoches_solucion = round(promedionoches_solucion,1)
+    return promedionoches_solucion
     
 
 btn = st.button("ENVIAR", on_click=clicked) 
